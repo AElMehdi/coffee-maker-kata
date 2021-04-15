bf02041700396e152d3cac30da70cf1ffb15a126
AElMehdi <assirelmehdi@gmail.com>
2021-04-15 20:33:26 +0200

    Order Translator: Check parsing of non integer value for sugar amount

--- src/test/java/OrderTranslatorTest.java
+++ src/test/java/OrderTranslatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OrderTranslatorTest {
 
@@ -34,4 +35,15 @@ class OrderTranslatorTest {
         assertThat(order).isEqualTo(new HotChocolateOrder(0));
     }
 
+    @Test
+    void should_return_a_hot_chocolate_order_with_sugar_when_passed_a_H_1_instruction() {
+        Order order = OrderTranslator.toOrder("H:1:");
+        assertThat(order).isEqualTo(new HotChocolateOrder(1));
+    }
+
+    @Test
+    void should_throw_an_exception_when_passed_an_alpha_as_amount_of_sugar() {
+        assertThatThrownBy(() -> OrderTranslator.toOrder("H:A:"))
+                .isInstanceOf(NumberFormatException.class);
+    }
 }
