fc7fe8c3e30d277cdb41b7cb5b036a9ba45ef2b5
AElMehdi <assirelmehdi@gmail.com>
2021-04-15 16:50:09 +0200

    UC: Make a Hot Chocolate with two sugars from an H:2 instruction

--- src/test/java/DrinkMakerTest.java
+++ src/test/java/DrinkMakerTest.java
@@ -23,12 +23,17 @@ public class DrinkMakerTest {
 
     @Test
     void should_make_a_hot_chocolate_when_receive_H() {
-        assertThat(drinkMaker.makeFrom("H")).isInstanceOf(HotChocolate.class);
+        assertThat(drinkMaker.makeFrom("H")).isEqualTo(new HotChocolate(0));
     }
 
    @Test
     void should_make_a_hot_chocolate_with_1_sugar_when_receive_H_1() {
         assertThat(drinkMaker.makeFrom("H:1")).isEqualTo(new HotChocolate(1));
+    }
+
+   @Test
+    void should_make_a_hot_chocolate_with_2_sugar_when_receive_H_2() {
+        assertThat(drinkMaker.makeFrom("H:2")).isEqualTo(new HotChocolate(2));
     }
 
     @Test
